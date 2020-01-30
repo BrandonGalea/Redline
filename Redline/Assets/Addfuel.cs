33101822bf8deb7705a30ac9b2c5cf4e591da356
Mightyyy <46237895+BrandonGalea@users.noreply.github.com>
2020-01-30 23:16:17 +0100

    Fuel script , fuel consumption  and main char
    updated the racer as the car tires were not hitting the ground , add fuel icon with consumption and added a fuel script to pick up cans and when car is moving fuel is lost.

---
+++ Redline/Assets/Addfuel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addfuel : MonoBehaviour{
+
+    public CarController carController;
+    
+    private void OnTriggerEnter2d(Collider2D collision)
+    {
+        carController.fuelconsumption = 1;
+        Destroy(gameObject);
+    }
+}
--- Redline/Assets/CarController.cs
+++ Redline/Assets/CarController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private float fuel = 1;
+    public float fuelconsumption = 0.1f;
     public Rigidbody2D carRigidbody;
     public Rigidbody2D backTire;
     public Rigidbody2D frontTire;
     public float speed = 20;
     private float carTorque = 10;
     private float movement;
+    public UnityEngine.UI.Image image;
 
 
     void Start()
@@ -20,13 +23,20 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
     }
 
     private void FixedUpdate()
     {
-        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-        carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        if(fuel>0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+        
+
+        fuel -= fuelconsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
     }
 }
 
